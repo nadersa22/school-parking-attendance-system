249250ba169e431dc83477586acc51a626980b62
nader serhal <naderserhal18@gmail.com>
2026-09-07 15:14:27 +0300

    refactor: remove role from registration request

--- backend/src/main/java/com/school/parkingattendance/dto/RegisterRequest.java
+++ backend/src/main/java/com/school/parkingattendance/dto/RegisterRequest.java
@@ -1,6 +1,5 @@
 package com.school.parkingattendance.dto;
 
-import com.school.parkingattendance.entity.Role;
 
 import lombok.Getter;
 import lombok.Setter;
@@ -12,5 +11,4 @@ public class RegisterRequest {
     private String name;
     private String email;
     private String password;
-    private Role role;
 }
